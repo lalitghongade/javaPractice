5ed6d44f1bc67e7c2bff347bb59eb143bd2df949
lalit ghongade <lalitghongade1451999@gmail.com>
2021-09-10 23:17:55 +0530

    reverse method added

--- Array/ArrayUtil.java
+++ Array/ArrayUtil.java
@@ -5,30 +5,59 @@ public class ArrayUtil{
 
     public void printArray(int [] arr){
             int n=arr.length;
+            System.out.print("Given Array : ");
             for (int i=0;i<n;i++){
-                System.out.print(arr[i]+"");
+                System.out.print(+arr[i]+" ");
             }
             System.out.println();
         }
 
-    public void arrayDemo(){
-        int [] arr={1,3,6,8,2,7};
-        System.out.println(findMisingNumber(arr));
-        printArray(arr);
-    }
+  
 
 
     public int findMisingNumber(int[] arr) {
         //this logic will be based on the number series given
         int n=arr.length +1;
-        int sum =n*(n+1)/2;
-        for(int num: arr)
-        {
+        int sum =n * (n+1) / 2;
+        //System.out.println(sum);
+        for(int num: arr){
             sum=sum-num;
         }
-
+       // System.out.println(sum);
         return sum;
     }
+
+    public void arrayDemo(){
+        int [] arr={1,3,6,8,2,4,7};
+        printArray(arr);
+        //System.out.println("Missing Number : "+findMisingNumber(arr));
+        System.out.println("----------------------------------------");
+        printArray(reverseArray(arr));
+        System.out.println("----------------------------------------");
+
+    }
+
+
+    private int[] reverseArray(int[] arr) {
+        int start=0,last=arr.length-1;
+        System.out.println("start:"+start+"last:"+last);
+        while(start<=last){
+            int temp=arr[start];
+            arr[start]=arr[last];
+            arr[last]=temp;
+            System.out.println("start "+arr[start]+""+"   last:"+arr[last]);
+            start++;
+            last--;
+        }
+
+
+
+        return arr;
+    }
+
+
+
+
     public static void main(String []args){
         ArrayUtil arrutil =new ArrayUtil();
         arrutil.arrayDemo();
